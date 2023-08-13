c4b404a978084fe06484fd91ff585c231b1e37fe
Douglas Pimentel <user.email>
2023-08-13 19:44:32 -0300

    add docker

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,11 +4,13 @@ dotenv.config();
 
 type ConfigType = {
   NODE_ENV: string;
+  HOST: string;
   PORT: number | string;
 };
 
 const config: ConfigType = {
   NODE_ENV: process.env.NODE_ENV || "development",
+  HOST: process.env.HOST || "127.0.0.1",
   PORT: process.env.PORT || 4000,
 };
 
