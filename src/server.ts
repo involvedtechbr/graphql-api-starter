5689ee21b773b7ea5b2c415684a38025f742b3bb
Douglas Pimentel <user.email>
2023-08-28 13:53:58 -0300

    feat: mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,20 @@
 import app from "./app";
 import config from "./config";
+import connectDB from "./database";
 
 (async () => {
   const PORT = config.PORT as number;
   const HOST = config.HOST;
-  const server = app;
+  const MONGO_URI: string = config.MONGO_URI;
 
   try {
-    await server.listen({ host: HOST, port: PORT });
+    await connectDB(MONGO_URI);
+    await app.listen({ host: HOST, port: PORT });
 
-    const address = server.server.address();
+    const address = app.server.address();
     const port = typeof address === "string" ? address : address?.port;
 
-    server.log.info("Port " + port);
+    app.log.info("Port " + port);
   } catch (err: unknown) {
     let messageError = "";
 
@@ -20,7 +22,7 @@ import config from "./config";
       messageError = err.message;
     }
 
-    server.log.error(messageError);
+    app.log.error(messageError);
     process.exit(1);
   }
 })();
