510d150d38d3e90eb92f380603db90d225f30afd
Douglas Pimentel <user.email>
2023-08-28 15:32:40 -0300

    encrypt and compare password

---
+++ src/utils/encryptPassword.ts
@@ -0,0 +1,20 @@
+import bcrypt from "bcrypt";
+import logger from "@/logger";
+
+async function encryptPassword(
+  password: string,
+  salt: number,
+): Promise<string> {
+  try {
+    const hash: string = await bcrypt.hash(password, salt);
+    return hash;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      logger.error(error.message);
+    }
+
+    throw new Error("Unable to encrypt the password");
+  }
+}
+
+export default encryptPassword;
