11e42b2fe1f22787d3f9f9d6d3bce7e9db0229c5
Douglas Pimentel <user.email>
2023-08-29 18:06:13 -0300

    update: query type

--- src/modules/user/userFields.ts
+++ src/modules/user/userFields.ts
@@ -1,10 +1,23 @@
-import { UserType } from "./UserType";
+import { UserType, UserConnection } from "./UserType";
 import { UserLoader } from "./UserLoader";
+import { connectionArgs } from "graphql-relay";
 
 export const userField = (key: string) => ({
-  [key]: {
-    type: UserType,
-    resolve: async (obj: Record<string, unknown>, _: unknown, context: any) =>
-      UserLoader.load(context, obj.user as string),
-  },
+	[key]: {
+		type: UserType,
+		resolve: async (obj: Record<string, unknown>, _: any, context: any) =>
+			await UserLoader.load(context, obj.user as string),
+	},
+});
+
+export const userConnectionField = (key: string) => ({
+	[key]: {
+		type: UserConnection.connectionType,
+		args: {
+			...connectionArgs,
+		},
+		resolve: async (_: any, args: any, context: any) => {
+			return await UserLoader.loadAll(context, args);
+		},
+	},
 });
