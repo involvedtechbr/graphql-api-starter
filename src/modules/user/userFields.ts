bffaf42175fb608ce166cadcf11d447da712faaf
Douglas Pimentel <user.email>
2023-08-30 12:25:01 -0300

    fix: github actions

--- src/modules/user/userFields.ts
+++ src/modules/user/userFields.ts
@@ -5,6 +5,7 @@ import { connectionArgs } from "graphql-relay";
 export const userField = (key: string) => ({
   [key]: {
     type: UserType,
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
     resolve: async (obj: Record<string, unknown>, _: any, context: any) =>
       await UserLoader.load(context, obj.user as string),
   },
@@ -16,6 +17,7 @@ export const userConnectionField = (key: string) => ({
     args: {
       ...connectionArgs,
     },
+    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
     resolve: async (_: any, args: any, context: any) => {
       return await UserLoader.loadAll(context, args);
     },
